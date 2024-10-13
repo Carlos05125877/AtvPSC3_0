a5e8fedc800389292a9bda0fc7d81c05a0700f9a
Carlos <carlos@gmail.com>
2024-10-13 14:48:46 -0300

    cc

--- Atv5e6/src/Medicos.java
+++ Atv5e6/src/Medicos.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Medicos{
     public static void cadastrarMedico(){
+        Scanner sc = new Scanner(System.in);
+        //Medico novoMedico = new Medico();
 
+        System.out.println("Nome Completo:");
+        String nomeMedico = sc.nextLine();
+        System.out.println("CRM:");
+        String crm = sc.nextLine();
+        System.out.println("Especialidade:");
+
+        String especialidade = sc.nextLine();
+        System.out.println("Telefone:");
+        String telefone = sc.nextLine();
+        System.out.println("Periodo de Atendimento:");
+        String periodoAtendimento = sc.nextLine();
+
+
+        System.out.println("CADASTRO REALIZADO!");
     }
 }
