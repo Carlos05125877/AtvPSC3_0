15bc94e49e6a224934559752796b9d3e3fd861e8
Carlos05125877 <126124305+Carlos05125877@users.noreply.github.com>
2024-10-16 20:24:34 -0300

    Update Paciente.java

--- Atv5e6/src/Paciente.java
+++ Atv5e6/src/Paciente.java
@@ -1,12 +1,21 @@
+import java.util.ArrayList;
 import java.util.Scanner;
-
 public class Paciente {
-
-    private int idPaciente;
+    private static ArrayList<Paciente> listaPacientes = new ArrayList<>();
     private String nomePaciente;
     private String dataNascimento;
     private String telefone;
     private String endereco;
+
+    public Paciente() {
+    }
+
+    public Paciente(String nomePaciente, String dataNascimento, String telefone, String endereco) {
+        this.nomePaciente = nomePaciente;
+        this.dataNascimento = dataNascimento;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
 
     public static void cadastrarPaciente(){
         Scanner sc = new Scanner(System.in);
@@ -20,8 +29,43 @@ public class Paciente {
         System.out.println("Endereço:");
         String endereco = sc.nextLine();
 
+        Paciente paciente = new Paciente(nomePaciente, dataNascimento, telefone, endereco);
+        listaPacientes.add(paciente);
 
         System.out.println("CADASTRO REALIZADO!");
     }
+    public static void ListaPacientes() {
+        if (listaPacientes.isEmpty()) {
+            System.out.println("Nenhum médico cadastrado!");
+            return;
+        }
 
+        System.out.println("\nLISTA DE MÉDICOS CADASTRADOS:");
+        for (Paciente paciente : listaPacientes) {
+            System.out.println("--------------------");
+            System.out.println("Nome: " + paciente.getNomePaciente());
+            System.out.println("CRM: " + paciente.getDataNascimento());
+            System.out.println("Telefone: " + paciente.getTelefone());
+            System.out.println("Período de Atendimento: " + paciente.getEndereco());
+
+        }
+    }
+
+
+
+    public String getNomePaciente() {
+        return nomePaciente;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
 }
